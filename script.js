d0ad4394000a83af57e2037dcb8793ce8b259003
Mike <michael.kerpelman@gmail.com>
2022-10-14 15:38:41 -0400

    Generate computer RPS choice with Math.random mapping between 0-2

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return "Rock"
+    break;
+    case 1:
+      return "Paper"
+    break;
+    case 2:
+      return "Scissors"
+    break;
+    default:
+      "Something went wrong."
+  }
+};
