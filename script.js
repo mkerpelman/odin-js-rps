9a6e82ffdb688b28f7807062d341ecc2b0d4a0a2
Mike <michael.kerpelman@gmail.com>
2022-10-15 10:02:32 -0400

    Perform a single round of RPS logic

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
+  let randomNumber = Math.floor(Math.random() * 3)
+  switch (randomNumber) {
+    case 0:
+      return "rock";
+    break;
+    case 1:
+      return "paper";
+    break;
+    case 2:
+      return "scissors";
+    break;
+    default:
+      console.log("Something went wrong, please try again.")
+  };
 };
 
 function getPlayerChoice() {
-  // Request string input from player
-  // Make string case insensitive
-  // Check that string is within choice
-  // Map string to integer 0-2
-  // Return numeric value
+  let playerInput = prompt("Rock, Paper, or Scissors?");
+  return playerInput.toLowerCase();
 };
 
-function evaluateChoices() {
-  
-}
+function evaluateChoices(playerChoice, computerChoice) {
+  let result = '';
+  if (playerChoice == computerChoice) {
+    result = 'tie';
+  } else if ((playerChoice == 'rock' && computerChoice == 'scissors') || (playerChoice == 'paper' && computerChoice == 'rock') || (playerChoice == 'scissors' && computerChoice == 'paper')) {
+    result = 'win';
+  } else {
+    result = 'lose';
+  };
+  return result;
+};
+
+function createOutcomeDialogue(roundOutcome, playerChoice, computerChoice) {
+  switch (roundOutcome) {
+    case 'win':
+      console.log(`Player wins! ${capitalizeFirstLetter(playerChoice)} beats ${computerChoice}.`);
+    break;
+    case 'lose':
+      console.log(`Computer wins! ${capitalizeFirstLetter(computerChoice)} beats ${playerChoice}.`);
+    break;
+    default:
+      console.log(`It's a tie! Both opponents played ${playerChoice}.`);
+  };
+};
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 function playRound() {
-  // Call function that gets player choice
-  // Call function that gets computer choice
-  // Call function that compares the two values
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const roundOutcome = evaluateChoices(playerChoice, computerChoice);
+  createOutcomeDialogue(roundOutcome, playerChoice, computerChoice);
 };
 
