3fc243b4b02bc6cfca15fe15823d12c19685209a
Mike <michael.kerpelman@gmail.com>
2022-10-15 08:41:02 -0400

    Write pseudocode for single round

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return "Rock"
-    break;
-    case 1:
-      return "Paper"
-    break;
-    case 2:
-      return "Scissors"
-    break;
-    default:
-      "Something went wrong."
-  }
+  return Math.floor(Math.random() * 3);
 };
+
+function getPlayerChoice() {
+  // Request string input from player
+  // Make string case insensitive
+  // Check that string is within choice
+  // Map string to integer 0-2
+  // Return numeric value
+};
+
+function evaluateChoices() {
+  
+}
+
+function playRound() {
+  // Call function that gets player choice
+  // Call function that gets computer choice
+  // Call function that compares the two values
+};
+
